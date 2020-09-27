dfeb9e131b72a898fb9311c73446f4085ca7fdd4
aqaqsubin <aqaq925120k@naver.com>
2020-09-27 19:57:27 +0900

    Add NodeJS Server Config

--- server_file/app_server.js
+++ server_file/app_server.js
@@ -20,17 +20,20 @@ mysqlConfig = {
     multipleStatements: true
 };
 
-app.use(bodyParser.json({ limit: '1000mb'}));
+app.use(bodyParser.json({ limit: '10000000mb'}));
+app.use(bodyParser.urlencoded({limit: '50000000', extended: true, parameterLimit:'500000'}));
 app.use(fileUpload());
 app.use(express.json())
 
 app.post('/upload/:id', function(req,res,next){
 	
 	var dir_path = "/home/una/Audio_For_Speaker-Identification-System-Development-By-Using-Deep-Learning/train/zipfiles/";
-	var obj = req.body;
-	var fileName = obj.fileName+".zip";
-	var str = obj.file;
-	var idx = null;
+        var target_path = "/home/una/Audio_For_Speaker-Identification-System-Development-By-Using-Deep-Learning/train/unzipfiles/";
+
+        var obj = req.body;
+        var fileName = obj.fileName+".zip";
+        var str = obj.file;
+        var idx = null;
 	
 	var options = {
 		mode: 'text',
@@ -42,60 +45,179 @@ app.post('/upload/:id', function(req,res,next){
 	}	
 	
 	if(str == null) return res.status(500).json({ result: "ERROR"});
+
+        console.log("Upload Files from "+obj.userName);
+        fs.writeFile(dir_path+fileName, str, {encoding:'base64'}, function(err){
+                if(err) {
+                        console.error(err);
+                        return res.status(500).json({ result : "ERROR", err : err});
+                }
+
+                console.log("File Created : "+ obj.fileName);
+
+
+                var c = mysql.createConnection(mysqlConfig);
+
+                c.query("SELECT idx FROM member_list ORDER BY idx DESC LIMIT 1",
+                        function(mysqlerr, row){
+                                if(mysqlerr){
+                                        console.error(mysqlerr);
+                                        return res.status(500).json({ result: "ERROR", err: mysqlerr});
+                                }
+                                if(row[0] == null) idx = 1;
+                                else{
+
+                                        idx = row[0].idx;
+                                        idx *= 1;
+                                        idx = idx +1;
+                                }
+                                console.log("idx : "+ idx);
+
+                                c.query("INSERT INTO member_list (member_key, name, path, idx) VALUES ( ?, ?, ?, ?)",
+                                [obj.fileName, obj.userName, target_path+obj.fileName, idx],
+                                        function(sql_inserr, mysqlres){
+                                                c.end();
+                                                if(sql_inserr){
+                                                console.error(sql_inserr);
+                                                return res.status(500).json({ result: "ERROR", err: sql_inserr});
+                                        }
+
+                                        var test = new PythonShell('zip_file_listener.py', options);
+                                        test.on('message', function(message){
+                                                console.log(message);
+                                        });
+                                        io.emit('message', JSON.stringify( [ obj.fileName,
+                                        JSON.stringify({ headers:req.headers, body: req.body})]));
+                                        return res.json({ result : "OK"});
+                                });
+
+                                return;
+
+                });
+
+
+        });
 	
-	fs.writeFile(dir_path+fileName, str, {encoding:'base64'}, function(err){
-		if(err) {
-			console.error(err);
-			return res.status(500).json({ result : "ERROR", err : err});
-		}
-		console.log("File Created");
-		
-		var t = new Date().getTime();
-		console.log("Time : "+t);
+});
+app.post('/identify', function(req,res,next){
 
-    		var label_num;
-		var c = mysql.createConnection(mysqlConfig);
-        
-        	c.query("SELECT idx FROM member_list ORDER BY idx DESC LIMIT 1",
-			function(mysqlerr, row){
-				if(mysqlerr){
-					console.error(mysqlerr);
-					return res.status(500).json({ result: "ERROR", err: mysqlerr});
-            			}
-            			if(isNaN(row[0])) idx = 1;
-				else{
+        var dir_path = "/home/una/Audio_For_Speaker-Identification-System-Development-By-Using-Deep-Learning/val/";
+        var model_path = "/home/una/Music/Speaker-Identification-System-Development-By-Using-Deep-Learning/System/SpeakerIdentificationSystem/"
+        var obj = req.body;
+        var fileName = obj.fileName+".zip";
+        var str = obj.file;
+        var idx = null;
 
-					idx = row[0].idx;
-            				idx *= 1;
-					idx = idx +1;
-				}
-				console.log("idx : "+ idx);
-			
-				c.query("INSERT INTO member_list (member_key, name, path, idx) VALUES ( ?, ?, ?, ?)",
-				[obj.fileName, obj.userName, dir_path+obj.fileName, idx],
-					function(sql_inserr, mysqlres){
-						c.end();
-						if(sql_inserr){
-						console.error(sql_inserr);
-						return res.status(500).json({ result: "ERROR", err: sql_inserr});
-					}
-					
-					var test = new PythonShell('zip_file_listener.py', options);
-					test.on('message', function(message){
-						console.log(message);
-					});
-					io.emit('message', JSON.stringify( [ obj.fileName, 
-					JSON.stringify({ headers:req.headers, body: req.body})]));
-					return res.json({ result : "OK"});
-				});
+        var options = {
+                mode: 'text',
+                encoding: 'utf8',
+                pythonOptions: ['-u'],
+                scriptPath: '/home/una/Music/Speaker-Identification-System-Development-By-Using-Deep-Learning/System/SpeakerIdentificationSystem/',
+                args: [obj.fileName, 2],
+                pythonPath:''
+        }
 
-				return;	
+        if(str == null) return res.status(500).json({ result: "ERROR"});
 
-		});
-			        
+        console.log("Identify User");
+        fs.writeFile(dir_path+fileName, str, {encoding:'base64'}, function(err){
+                if(err) {
+                        console.error(err);
+                        return res.status(500).json({ result : "ERROR", err : err});
+                }
+                console.log("File Created");
+                if(fs.existsSync(model_path)){
+                        // train
+                        var test = new PythonShell('zip_file_listener.py', options);
+                        test.on('message', function(message){
+                                console.log(message);
 
-	});
-	
+                        });
+
+                        io.emit('message', JSON.stringify( [ obj.fileName,
+                                JSON.stringify({ headers:req.headers, body: req.body})]));
+                        return res.json({ result : "OK"});
+
+                }else{
+                        return res.status(503).json({result : "TRAINING"});
+                }
+
+        });
+
+
+});
+app.post('/modifyName', function(req,res,next){
+
+        var obj = req.body;
+        var dir_path = "/home/una/Audio_For_Speaker-Identification-System-Development-By-Using-Deep-Learning/train/unzipfiles/";
+
+        var fileName = obj.fileName;
+        var changedName = obj.changedName;
+
+
+        if(!fs.existsSync(dir_path+fileName)) return res.status(501).json({result : "ERROR"});
+        console.log("Modify User Name");
+
+        var c = mysql.createConnection(mysqlConfig);
+
+        c.query("UPDATE member_list SET name = ? WHERE member_key = ?",
+                [obj.changedName, fileName],
+                function(sql_inserr, mysqlres){
+                        c.end();
+                        if(sql_inserr){
+                                console.error(sql_inserr);
+                                return res.status(500).json({ result: "ERROR", err: sql_inserr});
+                        }
+
+                        io.emit('message', JSON.stringify( [ obj.fileName,
+                        JSON.stringify({ headers:req.headers, body: req.body})]));
+                        return res.json({ result : "OK"});
+                });
+
+
+});
+app.post('/modifyVoice', function(req,res,next){
+
+        var dir_path = "/home/una/Audio_For_Speaker-Identification-System-Development-By-Using-Deep-Learning/train/zipfiles/";
+        var target_path = "/home/una/Audio_For_Speaker-Identification-System-Development-By-Using-Deep-Learning/train/unzipfiles/";
+
+        var obj = req.body;
+        var fileName = obj.fileName+".zip";
+        var str = obj.file;
+        var idx = null;
+
+        var options = {
+                mode: 'text',
+                encoding: 'utf8',
+                pythonOptions: ['-u'],
+                scriptPath: '/home/una/Music/Speaker-Identification-System-Development-By-Using-Deep-Learning/System/SpeakerIdentificationSystem/',
+                args: [obj.fileName, 3],
+                pythonPath:''
+        }
+
+        if(str == null) return res.status(500).json({ result: "ERROR"});
+        console.log("Modify User Voice");
+        fs.writeFile(dir_path+fileName, str, {encoding:'base64'}, function(err){
+                if(err) {
+                        console.error(err);
+                        return res.status(500).json({ result : "ERROR", err : err});
+                }
+
+                console.log("File Created : "+ obj.fileName);
+
+
+                var test = new PythonShell('zip_file_listener.py', options);
+                test.on('message', function(message){
+                        console.log(message);
+                });
+                io.emit('message', JSON.stringify( [ obj.fileName,
+                JSON.stringify({ headers:req.headers, body: req.body})]));
+                return res.json({ result : "OK"});
+
+
+        });
+
+
 });
 
 app.get('/', (req, res) => {
--- server_file/zip_file_listener.py
+++ server_file/zip_file_listener.py
@@ -1,8 +1,20 @@
 import threading as th
 import os
 import zipfile
+import sys
+from shutil import rmtree
+from run import upload
+from run import identify
+#from run import reupload
 
-file_path_name = '/home/una/Audio_For_Speaker-Identification-System-Development-By-Using-Deep-Learning/train/zipfiles/'
+zipfile_name = sys.argv[1]
+flag = sys.argv[2]
+
+print(zipfile_name, flag)
+
+dir_name = '/home/una/Audio_For_Speaker-Identification-System-Development-By-Using-Deep-Learning/train/'
+zipfile_path_name = dir_name + 'zipfiles/'
+unzipfile_path_name = dir_name + 'unzipfiles/'
 
 prev_file_list = []
 prev_file_num = len(prev_file_list)
@@ -22,25 +34,34 @@ def mkdir_p(path):
 def del_folder(path):
     try:
         rmtree(path)
-    except:
+    except OSError:
         pass
 
+le True:
+    upload_filename = zipfile_path_name + zipfile_name
+    batch_filename = unzipfile_path_name + zipfile_name
 
-while True:
-    cur_file_list = os.listdir(file_path_name)
-    cur_file_list.sort()
-    for i in range(len(cur_file_list)):
-        try:
-            if (len(cur_file_list[i]) != 32):
-                file_name = file_path_name + cur_file_list[i]
-                print(file_name)
-                all_info_zip = zipfile.ZipFile(file_name)
-                mkdir_p(file_path_name + cur_file_list[i][:-4])
-                all_info_zip.extractall(file_name[:-4])
-                all_info_zip.close()
-                os.remove(file_name)
-        except:
-                print("File Error !")
-                print(cur_file_list[i])
-                break
+    try:
+        print(batch_filename)
+        all_info_zip = zipfile.ZipFile(upload_filename + ".zip")
+
+        mkdir_p(batch_filename + "/origin")
+        all_info_zip.extractall(batch_filename + "/origin")
+        all_info_zip.close()
+
+        os.remove(upload_filename + ".zip")
+        break
+    except Exception as ex:
+        print("File Error !", ex)
+        print(upload_filename)
+        pass
+
+if int(flag) == 1:
+    print("upload function call")
+    upload()
+elif int(flag) == 2:
+    print("check user")
+    identify()
+#else :
+#    reupload(zipfile_name)
 
